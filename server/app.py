9fafffd0fa7f72357485566b46ae82c2f3a31343
Martin Tóth <ezimir@gmail.com>
2014-12-17 18:24:00 +0100

    Base template and mockup of menu items
    These are used to ease CSS work:
     - https://github.com/necolas/normalize.css
     - https://github.com/dhg/Skeleton
     - https://github.com/hakimel/Ladda

--- server/app.py
+++ server/app.py
@@ -2,13 +2,42 @@
 
 
 
+# functions
+from flask import render_template
+
+# objects
 from flask import Flask
+
 
 
 app = Flask(__name__)
 
 
+
+@app.context_processor
+def inject_user():
+    return {
+        'user': None,
+    }
+
+@app.context_processor
+def inject_menu():
+    return {
+        'menu': [
+            ('home', 'Home'),
+            ('home', 'Bookmarks'),
+            ('home', 'View Data'),
+            ('home', 'Logout'),
+        ],
+    }
+
+
 @app.route('/')
 def home():
-    return u'Gmail Connections'
+    return render_template('home.html')
+
+
+# dev server
+if __name__ == '__main__':
+    app.run(debug = True)
 
